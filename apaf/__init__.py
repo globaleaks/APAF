6cbf3fb8df09a463f01a4c11b251d4d6cb1cea1f
Michele Orrù <maker.py@gmail.com>
2012-05-20 12:27:48 +0200

    typo in hiddenservices variable.

--- apaf/__init__.py
+++ apaf/__init__.py
@@ -1,8 +1,9 @@
 # -*- coding : UTF-8 -*-
 __version__ = '0x0.1'
-__author__ = u'Michele Orr\xc3\xb9 <maker.py@gmail.com>'
+__author__ = u'Michele Orr\xc3\xb9'
+__email__ = 'maker.py@gmail.com'
+__license__ = """
+"""
 
-
-
-# hidden services kept at global state
-hiddenservices = []
+# hidden
+hiddeservices = []
