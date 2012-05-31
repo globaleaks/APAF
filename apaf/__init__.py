42e30c6085dbc5f635ef2c80c3b85f8d8b092d5f
Michele Orrù <maker.py@gmail.com>
2012-05-31 22:51:02 +0200

    Adding license.

--- apaf/__init__.py
+++ apaf/__init__.py
@@ -3,7 +3,21 @@ __version__ = '0x0.1'
 __author__ = u'Michele Orr\xc3\xb9'
 __email__ = 'maker.py@gmail.com'
 __license__ = """
+            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+                    Version 2, December 2004
+
+ Copyright (C) 2004 Sam Hocevar <sam@hocevar.net>
+
+ Everyone is permitted to copy and distribute verbatim or modified
+ copies of this license document, and changing it is allowed as long
+ as the name is changed.
+
+            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+
+  0. You just DO WHAT THE FUCK YOU WANT TO.
 """
+
 
 # service : hidden service map.
 hiddenservices = dict()
