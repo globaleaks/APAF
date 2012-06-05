cd9010ee2bf07c690547402cd5c544332b55b35e
mogui <mogui83@gmail.com>
2012-06-05 21:09:52 +0200

    modified build instructions for os x and added script to automate build

---
+++ os_x_build_setup.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#
+# APAF build system OS X install script
+#
+
+if ! which git; then
+	#exit if we do not have git
+	echo "Couldn't start without git please download it from: http://git-scm.com/download/mac"
+	exit 1;
+fi
+
+if ! which pip &> /dev/null; then
+    echo "Installing pip"
+    python easy_install pip
+fi
+
+if ! which virtualenv &> /dev/null; then
+    echo "Installing virtual env"
+    python easy_install virtualenv
+fi
+
+# setup env
+virtualenv apaf-env
+. apaf-env/bin/activate
+echo `which python`
+
+
+pip install psutil py2app twisted six pygeoip ipaddr pyYAML
+easy_install http://pypi.python.org/packages/source/z/zope.interface/zope.interface-4.0.0.tar.gz
+
+mkdir src
+cd src
+
+git clone https://github.com/mmaker/txtorcon.git
+pip install ./txtorcon
+
+#download tor browser to extract executable
+wget --no-check-certificate https://www.torproject.org/dist/torbrowser/osx/TorBrowser-2.2.35-12-osx-i386-en-US.zip
+unzip TorBrowser-2*
+
+# move up
+cd ..
+mv TorBrowser_en-US.app/Contents/MacOS/tor datadir/contrib/
+rm -rf src/
+
+
+
+
+
