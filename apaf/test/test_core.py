854b55e2b9080a895d5637cb78de1b0585a91c39
Michele Orrù <maker.py@gmail.com>
2012-08-10 09:10:11 +0200

    unit-testing interface for services.

--- apaf/test/test_core.py
+++ apaf/test/test_core.py
@@ -1,3 +1,4 @@
+from zope.interface import verify
 from twisted.trial import unittest
 
 from apaf import core, config
@@ -26,7 +27,12 @@ class TestService(unittest.TestCase):
         self.assertNotEqual(core.new_port(), core.new_port())
         self.assertGreater(core.new_port(), config.custom['base_port'])
 
-
+    def test_iservice(self):
+        """
+        Assert the implementation of Service is compatible with its interface
+        IService.
+        """
+        self.assertTrue(verify.verifyObject(core.IService, core.Service()))
 
 if __name__ == '__main__':
     from unittest import main
