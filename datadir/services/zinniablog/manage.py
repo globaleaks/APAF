3601c03a5cff634ed4faec48ae0698ad6df0ec8b
Michele Orrù <maker.py@gmail.com>
2012-10-21 17:01:46 +0200

    Importing @djon3s patch

--- datadir/services/zinniablog/manage.py
+++ datadir/services/zinniablog/manage.py
@@ -2,6 +2,10 @@
 import os
 import sys
 
+# append apaf to the current $PYTHONPATH
+sys.path.append(os.path.abspath(os.path.join('..', '..', '..')))
+
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blog.settings")
     from django.core.management import execute_from_command_line
